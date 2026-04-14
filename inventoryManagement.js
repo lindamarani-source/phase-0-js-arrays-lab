940296a9febe70e37602c98178db780027b1571c
lindamarani-source <linda.marani@student.moringaschool.com>
2026-04-14 07:46:35 +0300

    Commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,22 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  function logFirstProduct() {
+    console.log(products[0]);
+  }
+}
 
+function updateProductName(index, newName) {
+const oldName = products[index];
+products[index] = newName;
+console.log(`Updated product at index ${index} from "${oldName}" to "${newName}".`);
+}
+
+function removeLastProduct() {
+  products.pop();
+  console.log("products");
+}
 
 // Export the necessary parts for testing
 module.exports = {
